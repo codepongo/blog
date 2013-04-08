fcebfdfe371ea71684c42c47e4b3d0b95cce5a8f
zuoht <zuoht@rising.com.cn>
2013-04-08 16:22:29 +0800

    donate paypal

--- blog.py
+++ blog.py
@@ -1013,8 +1013,15 @@ def renderAdvert():
 
 def renderSidebarDonate():
     print "<h2>%s</h2>" % l_donate
-    print "<a href='http://me.alipay.com/codepongo'><img src='https://img.alipay.com/sys/personalprod/style/mc/btn-index.png' /></a>"
+    print '<a href="http://me.alipay.com/codepongo"><img src="https://img.alipay.com/sys/personalprod/style/mc/btn-index.png" /></a>'
     print "<br />"
+    print """\
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="WD22V8UQFPCZE">
+<input type="image" src="http://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>"""
 def renderSidebarCategories(catelist, rss_categories):
     categories = catelist.keys()
     try:
