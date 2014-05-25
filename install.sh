e69daca46f22cf1fbcd1a7a04d5d06d94a8020bf
zhuhuotui@mac.home <zhuhuotui@gmail.com>
2014-05-25 23:11:13 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #html
-if [! -d $1];then
+if [ ! -d "$1" ];then
 	mkdir $1
 fi
 cp *.jpg $1
@@ -9,13 +9,13 @@ cp *.css $1
 cp *.ico $1
 cp -r rainbow $1
 #cgi-bin
-if [! -d $2];then
+if [ ! -d "$2" ];then
 	mkdir $2
 fi
 cp *.py $2
 cp -r pyftpdlib $2
 #data 
-if [! -d $3]; then
+if [ ! -d "$3" ]; then
 	mkdir $3
 fi
 cp *.md $3
